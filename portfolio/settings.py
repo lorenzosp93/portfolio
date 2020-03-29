d2b55771c48e3c0bf8e90e9985d947d8d2fd325c
lorenzosp93 <lorenzosp93@gmail.com>
2020-03-30 00:18:00 +0200

    fix settings static_root

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -157,9 +157,9 @@ if USE_S3:
 
 else:
     STATIC_URL = '/static/'
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'mediafiles')
 
