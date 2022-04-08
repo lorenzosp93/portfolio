ba83d78a33a925e0f59eba069a767a524d66e135
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-04-08 20:33:53 +0200

    move about text to front end

--- portfolio-frontend/src/components/UI/TheHero.vue
+++ portfolio-frontend/src/components/UI/TheHero.vue
@@ -2,7 +2,7 @@
   <div class="flex min-h-screen flex-wrap m-5 snap-start snap-always justify-self-center text-black dark:text-white">
     
     <div class="flex-initial w-1/2 md:w-1/3 m-auto" >
-      <img class='m-auto ring-4 ring-white z-20 relative rounded-full' id="heroPicture" src="@/assets/hero.jpeg" :class="{'invisible': !isVisible}" alt="High res picture" @load="imageIsLoaded">
+      <img class='m-auto ring-4 ring-white z-20 relative rounded-full' id="heroPicture" src="@/assets/hero.jpeg" :class="{'invisible': !isVisible}" alt="High res picture" @load="$emit('heroLoaded')">
       <h1 class="text-2xl m-5 font-bold text-center">Hi, I'm Lorenzo</h1>
       <div class="container flex my-5">
         <a class=" fill-gray-700 dark:fill-white ml-auto" href="https://twitter.com/Lorenzosp">
@@ -19,18 +19,26 @@
     <div></div>
     <div class="flex-initial w-2/3 md:w-1/2 mt-auto m-auto">
       <div class="font-serif first-letter:text-2xl justify-between m-3 indent-5">
-        <p v-if="data.length > 0" v-html="data[0].about_text"/>
-        <div class="p-10 m-auto" v-else-if="isLoading">
-          <svg role="status" class="mx-auto w-10 h-10 text-gray-200 animate-spin dark:text-gray-400 fill-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-              <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-          </svg>
-        </div>
-        <div @click="loadSettings" v-else class="rounded-3xl p-3 bg-gray-300 dark:bg-gray-500">
-          Unable to load, click to retry.
-        </div>
+        <h1 class="text-lg my-3">Welcome to my website!</h1>
+        <p> I'm an energy engineer by formal training, jack of all trades by attitude and development. I work on Product at Tesla, aiming to always learn and grow into the role to acquire the skills and knowledge required to run a business and eventually move into VC.</p>
+        <h3 class="text-xl my-3">Why a porfolio website?</h3>
+        <p>
+          <ul class="list-disc list-inside">
+            <li>This has been great fun to build!
+            </li>
+            <li>To get my hands dirty with the technologies in web development, including: 
+              <ul class="list-disc ml-5 list-inside">
+                <li> a Django REST API to serve the website content,</li>
+                <li> a Vue.js front-end, enhanced with plenty of cool animations and touch-effects,</li>
+                <li> a state-of-the-art Continuous Integration / Continuous Development pipeline, based on Docker containers running in Kubernetes, and taking advantage of load balancing, autoscaling, and more!</li>
+              </ul>
+            </li>
+            <li>To have a nice <a href="#blog">place</a> to write whatever comes to mind</li>
+            <li>To extend and supplement my Linkedin profile</li>
+          </ul>
+        </p>
       </div>
-    </div>
+      </div>
   </div>
 </template>
 
@@ -46,16 +54,6 @@ export default {
       aboutLoaded: false,
     }
   },
-  watch: {
-    isLoading (newValue, oldValue) {
-      if (oldValue) {
-        this.aboutLoaded = true;
-        if (this.imageLoaded) {
-          this.$emit('heroLoaded');
-        }
-      }
-    }
-  },
   props: {
     observer: Object,
     elementsInView: Array,
@@ -64,7 +62,6 @@ export default {
     'heroLoaded',
   ],
   inject: [
-    'loadData'
   ],
   computed: {
     isVisible () {
@@ -73,20 +70,7 @@ export default {
       return !e.filter(elem => elem.intersectionRatio > 0)?.length == 0
     }
   },
-  methods:{
-    loadSettings () {
-      let url = '/api/site/settings/';
-      this.loadData(url, this);
-    },
-    imageIsLoaded () {
-      this.imageLoaded = true;
-      if (this.aboutLoaded) {
-        this.$emit('heroLoaded');
-      }
-    }
-  },
   mounted () {
-    this.loadSettings()
     this.observer.observe(this.$el);
   }
 }
