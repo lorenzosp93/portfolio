0017feb5f6a7e50ab32f0020cee3d6040901e0b4
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-04-08 16:47:13 +0200

    downgrade django - csrf bug

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -63,7 +63,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
