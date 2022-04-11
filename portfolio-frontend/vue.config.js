5f65496f4a18221154dbb5dea8768415c9d3735e
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-04-11 14:18:53 +0200

    add compression

--- portfolio-frontend/vue.config.js
+++ portfolio-frontend/vue.config.js
@@ -1,8 +1,31 @@
 const { defineConfig } = require('@vue/cli-service')
+const zlib = require("zlib");
+
 
 module.exports = defineConfig({
   configureWebpack: {
     devtool: 'source-map'
   },
-  transpileDependencies: true
+  transpileDependencies: true,
+  pluginOptions: {
+    compression:{
+      brotli: {
+        filename: '[file].br[query]',
+        algorithm: 'brotliCompress',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        compressionOptions: {
+          params: {
+            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
+          },
+        },
+        minRatio: 0.8,
+      },
+      gzip: {
+        filename: '[file].gz[query]',
+        algorithm: 'gzip',
+        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
+        minRatio: 0.8,
+      }
+    }
+  }
 })
