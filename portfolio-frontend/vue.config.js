e2a9ec45e05e3f181394829fa8b6b569819446ec
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-04-11 15:24:28 +0200

    fix backend url

--- portfolio-frontend/vue.config.js
+++ portfolio-frontend/vue.config.js
@@ -1,6 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
-const zlib = require("zlib");
-
 
 module.exports = defineConfig({
   pages: {
@@ -14,25 +12,4 @@ module.exports = defineConfig({
     devtool: 'source-map'
   },
   transpileDependencies: true,
-  pluginOptions: {
-    compression:{
-      brotli: {
-        filename: '[file].br[query]',
-        algorithm: 'brotliCompress',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-        compressionOptions: {
-          params: {
-            [zlib.constants.BROTLI_PARAM_QUALITY]: 11,
-          },
-        },
-        minRatio: 0.8,
-      },
-      gzip: {
-        filename: '[file].gz[query]',
-        algorithm: 'gzip',
-        include: /\.(js|css|html|svg|json)(\?.*)?$/i,
-        minRatio: 0.8,
-      }
-    }
-  }
 })
