e099f9a588e2d092bb0c84268b15306cd8b667a1
Lorenzo Spinelli <lorenzosp93@gmail.com>
2022-11-26 15:39:16 +0100

    fix https

--- portfolio-backend/portfolio/settings.py
+++ portfolio-backend/portfolio/settings.py
@@ -209,3 +209,5 @@ EMAIL_PORT = os.environ.get('EMAIL_PORT', 587)
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', "True") == "True"
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
