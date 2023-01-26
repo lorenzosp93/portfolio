69a161056d856360baad6294ea72da841c46f794
Lorenzo Spinelli <lorenzosp93@gmail.com>
2023-01-26 09:54:45 +0100

    improve icons

--- portfolio-frontend/src/components/blog/TheBlog.vue
+++ portfolio-frontend/src/components/blog/TheBlog.vue
@@ -1,105 +1,150 @@
 <template>
-  <div class="container min-h-[55vh] lg:min-h-[60vh] relative flex w-screen mx-auto flex-wrap">
-    <div class="class container flex flex-wrap mx-auto my-10">
-      <h2 class="w-full mt-auto text-center text-xl font-bold  text-gray-600 dark:text-white">
-        Thoughts from the blog.
-      </h2>
-      <p class="w-full mb-auto text-center text-gray-600 dark:text-gray-300">
-        Who needs MySpace when you can create your own blog and write whatever comes to mind on it, right?
-      </p>
-    </div>
-    <div v-if="data.length && scrollPosition != 'begin'" @click="scrollToSibling(false)" class="z-10 absolute -left-5 top-1/2 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden md:block hover:bg-gray-100 hover:dark:bg-gray-400 cursor-pointer select-none">
-        <div class="translate-y-full translate-x-1/4 h-1/3 w-1/3 mx-auto rotate-45 border-b-2 border-l-2 border-gray-600 dark:border-gray-900 box-border rounded-bl" ></div>
-    </div>
-    <div v-if="data.length && scrollPosition != 'end'" @click="scrollToSibling(true)" class="z-10 absolute -right-5 top-1/2 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden md:block hover:dark:bg-gray-400 hover:bg-gray-100 cursor-pointer select-none">
-        <div class="translate-y-full -translate-x-1/4 h-1/3 w-1/3 mx-auto rotate-45 border-t-2 border-r-2 border-gray-600 dark:border-gray-900 box-border rounded-tr" ></div>
-    </div>
-    <div class="p-auto gap-6 container relative w-screen flex overflow-x-scroll no-scrollbar snap-x snap-mandatory m-auto scroll-smooth scroll-px-1 p-5" id="blog-container">
-        <list-card type="blog" class="blog-card w-3/5 md:w-1/3 lg:w-1/4" v-for="post in data" :key="post?.uuid" v-bind="post" :isActive="isActive"/>
-      <div class="snap-center relative w-10 h-10 p-6 my-auto mx-10 flex bg-white dark:bg-gray-900 shadow-md container flex-none rounded-full" v-if="isLoading">
-        <svg role="status" class="absolute left-1 top-1 w-10 h-10 text-gray-100 animate-spin dark:text-gray-400 fill-gray-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
-            <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
-        </svg>
-      </div>
-      <retry-button v-else-if="!data.length" @load-entries="loadEntries" />
-    </div>
-  </div>
+	<div
+		class="container min-h-[55vh] lg:min-h-[60vh] relative flex w-screen mx-auto flex-wrap"
+	>
+		<div class="class container flex flex-wrap mx-auto my-10">
+			<h2
+				class="w-full mt-auto text-center text-xl font-bold text-gray-600 dark:text-white"
+			>
+				Thoughts from the blog.
+			</h2>
+			<p
+				class="w-full mb-auto text-center text-gray-600 dark:text-gray-300"
+			>
+				Who needs MySpace when you can create your own blog and write
+				whatever comes to mind on it, right?
+			</p>
+		</div>
+		<div
+			v-if="data.length && scrollPosition != 'begin'"
+			@click="scrollToSibling(false)"
+			class="z-10 absolute -left-5 top-1/2 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden md:block hover:bg-gray-100 hover:dark:bg-gray-400 cursor-pointer select-none"
+		>
+			<chevron-left-icon
+				class="w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+			></chevron-left-icon>
+		</div>
+		<div
+			v-if="data.length && scrollPosition != 'end'"
+			@click="scrollToSibling(true)"
+			class="z-10 absolute -right-5 top-1/2 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden md:block hover:dark:bg-gray-400 hover:bg-gray-100 cursor-pointer select-none"
+		>
+			<chevron-right-icon
+				class="w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+			></chevron-right-icon>
+		</div>
+		<div
+			class="p-auto gap-6 container relative w-screen flex overflow-x-scroll no-scrollbar snap-x snap-mandatory m-auto scroll-smooth scroll-px-1 p-5"
+			id="blog-container"
+		>
+			<list-card
+				type="blog"
+				class="blog-card w-3/5 md:w-1/3 lg:w-1/4"
+				v-for="post in data"
+				:key="post?.uuid"
+				v-bind="post"
+				:isActive="isActive"
+			/>
+			<div
+				class="snap-center relative w-10 h-10 p-6 my-auto mx-10 flex bg-white dark:bg-gray-900 shadow-md container flex-none rounded-full"
+				v-if="isLoading"
+			>
+				<svg
+					role="status"
+					class="absolute left-1 top-1 w-10 h-10 text-gray-100 animate-spin dark:text-gray-400 fill-gray-600"
+					viewBox="0 0 100 101"
+					fill="none"
+					xmlns="http://www.w3.org/2000/svg"
+				>
+					<path
+						d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+						fill="currentColor"
+					/>
+					<path
+						d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+						fill="currentFill"
+					/>
+				</svg>
+			</div>
+			<retry-button
+				v-else-if="!data.length"
+				@load-entries="loadEntries"
+			/>
+		</div>
+	</div>
 </template>
 
 <script>
-import ListCard from '../UI/Card/ListCard.vue';
-import RetryButton from '../UI/Buttons/RetryButton.vue';
+import ListCard from "../UI/Card/ListCard.vue";
+import RetryButton from "../UI/Buttons/RetryButton.vue";
+import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/vue/24/outline";
 
 export default {
-  components: {ListCard, RetryButton},
-  name: 'TheBlog',
-  data () {
-    return {
-      isLoading: false,
-      error: false,
-      data: [],
-      total: 0,
-      scrollPosition: 'begin',
-    }
-  },
-  watch: {
-    isActive (value) {
-      if (value && this.data.length == 0 && !this.isLoading) {
-        this.isLoading = true;
-        this.loadEntries();
-      }
-    },
-  },
-  props: [
-    "isActive",
-    "observer",
-  ],
-  inject: [
-    'loadData',
-    'entriesLimit',
-  ],
-  computed: {
-  },
-  methods: {
-    loadEntries () {
-      this.isLoading = true;
-      let url = `/api/blog/post/?limit=${this.entriesLimit()}&offset=${this.data.length}`;
-      this.loadData(url, this, true);
-    },
-    scrollToSibling (next) {
-      let scrollWidth = document.getElementsByClassName('blog-card')[0].clientWidth;
-      let container = document.getElementById('blog-container');
-      if (next) {
-        container.scrollLeft += scrollWidth;
-      } else {
-        container.scrollLeft -= scrollWidth;
-      }
-    }
-  },
-  beforeUnmount () {
-  },
-  mounted () {
-    this.observer.observe(this.$el);
-    let container = document.getElementById('blog-container')
-    container.addEventListener("scroll", () => { 
-      let currentLength = this.data.length;
-      if (container.scrollLeft == 0) {
-        this.scrollPosition = 'begin';
-      } else if (container.scrollLeft == container.scrollWidth - container.offsetWidth) {
-        this.scrollPosition = 'end';
-        if ((this.total > currentLength) && !this.isLoading){
-          this.isLoading = true;
-          this.loadEntries();
-        }
-      } else {
-        this.scrollPosition = null;
-      }
-    }) 
-  }
-}
+	components: { ListCard, RetryButton, ChevronLeftIcon, ChevronRightIcon },
+	name: "TheBlog",
+	data() {
+		return {
+			isLoading: false,
+			error: false,
+			data: [],
+			total: 0,
+			scrollPosition: "begin",
+		};
+	},
+	watch: {
+		isActive(value) {
+			if (value && this.data.length == 0 && !this.isLoading) {
+				this.isLoading = true;
+				this.loadEntries();
+			}
+		},
+	},
+	props: ["isActive", "observer"],
+	inject: ["loadData", "entriesLimit"],
+	computed: {},
+	methods: {
+		loadEntries() {
+			this.isLoading = true;
+			let url = `/api/blog/post/?limit=${this.entriesLimit()}&offset=${
+				this.data.length
+			}`;
+			this.loadData(url, this, true);
+		},
+		scrollToSibling(next) {
+			let scrollWidth =
+				document.getElementsByClassName("blog-card")[0].clientWidth;
+			let container = document.getElementById("blog-container");
+			if (next) {
+				container.scrollLeft += scrollWidth;
+			} else {
+				container.scrollLeft -= scrollWidth;
+			}
+		},
+	},
+	beforeUnmount() {},
+	mounted() {
+		this.observer.observe(this.$el);
+		let container = document.getElementById("blog-container");
+		container.addEventListener("scroll", () => {
+			let currentLength = this.data.length;
+			if (container.scrollLeft == 0) {
+				this.scrollPosition = "begin";
+			} else if (
+				container.scrollLeft ==
+				container.scrollWidth - container.offsetWidth
+			) {
+				this.scrollPosition = "end";
+				if (this.total > currentLength && !this.isLoading) {
+					this.isLoading = true;
+					this.loadEntries();
+				}
+			} else {
+				this.scrollPosition = null;
+			}
+		});
+	},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-</style>
+<style scoped></style>
--- portfolio-frontend/src/components/resume/TheResume.vue
+++ portfolio-frontend/src/components/resume/TheResume.vue
@@ -1,140 +1,197 @@
 <template>
-<div class="container relative min-h-screen w-screen mx-auto mt-10">
-  <div class="class container flex flex-wrap mx-auto mb-10">
-    <h2 class="text-center text-xl w-full font-bold mx-auto  text-gray-600 dark:text-white">
-      Here are a few things I've done.
-    </h2>
-    <p class="text-center w-full text-gray-600 dark:text-gray-300">
-      Because I definitely needed a website to host my CV. <span class="md:hidden"> Swipe horizontally to change
-        section!</span>
-    </p>
-  </div>
-  <div class="z-10 sticky top-1/2 hidden md:block max-w-screen-lg mx-auto opacity-0" id="arrow-holder-resume">
-    <div @click="scrollToSibling(false)"
-      class="absolute -left-5 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden hover:bg-gray-100 hover:dark:bg-gray-400 cursor-pointer select-none"
-      :class="{'md:block': !isExperienceActive}">
-      <div
-        class="translate-y-full translate-x-1/4 h-1/3 w-1/3 mx-auto rotate-45 border-b-2 border-l-2 border-gray-600 dark:border-gray-900 box-border rounded-bl">
-      </div>
-    </div>
-    <div @click="scrollToSibling(true)"
-      class="absolute -right-5 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden hover:dark:bg-gray-400 hover:bg-gray-100 cursor-pointer select-none"
-      :class="{'md:block': !isSkillsActive}">
-      <div
-        class="translate-y-full -translate-x-1/4 h-1/3 w-1/3 mx-auto rotate-45 border-t-2 border-r-2 border-gray-600 dark:border-gray-900 box-border rounded-tr">
-      </div>
-    </div>
-  </div>
-  <div @click="scrollToTop"
-    class="absolute right-10 bottom-3 z-10 h-10 w-10 rounded-full shadow-md bg-gray-50 dark:bg-gray-500 cursor-pointer select-none hover:dark:bg-gray-400 hover:bg-gray-100"
-    id="top-scroller-resume">
-    <div
-      class="translate-y-[1em]  h-1/3 w-1/3 mx-auto rotate-45 border-t-2 border-l-2 border-gray-600 dark:border-gray-900 box-border rounded-lt">
-    </div>
-  </div>
-  <div class="container relative flex gap-6 overflow-x-scroll no-scrollbar snap-x snap-mandatory scroll-smooth w-full"
-    id="resume-container">
-    <div class="flex-none w-full grow snap-center resume-panels">
-      <resume-timeline :ix="'first'" :observer="observer" :isActive="isExperienceActive" :kind="'experience'"
-        id="experience" class="scroll-my-10" @load-complete="refreshAnimations" />
-    </div>
-    <div class="flex-none w-full grow snap-center resume-panels">
-      <resume-timeline :ix="'center'" :observer="observer" :isActive="isEducationActive" :kind="'education'"
-        id="education" class="scroll-my-10" @load-complete="refreshAnimations" />
-    </div>
-    <div class="flex-none w-full grow snap-center resume-panels">
-      <resume-projects :ix="'center'" :observer="observer" id="projects" :isActive="isProjectsActive" class="scroll-my-10" @load-complete="refreshAnimations" />
-    </div>
-    <div class="flex-none w-full grow snap-center resume-panels">
-      <resume-skills :ix="'last'" :observer="observer" id="skills" :isActive="isSkillsActive" class="scroll-my-10"/>
-    </div>
-  </div>
-</div>
+	<div class="container relative min-h-screen w-screen mx-auto mt-10">
+		<div class="class container flex flex-wrap mx-auto mb-10">
+			<h2
+				class="text-center text-xl w-full font-bold mx-auto text-gray-600 dark:text-white"
+			>
+				Here are a few things I've done.
+			</h2>
+			<p class="text-center w-full text-gray-600 dark:text-gray-300">
+				Because I definitely needed a website to host my CV.
+				<span class="md:hidden">
+					Swipe horizontally to change section!</span
+				>
+			</p>
+		</div>
+		<div
+			class="z-10 sticky top-1/2 hidden md:block max-w-screen-lg mx-auto opacity-0"
+			id="arrow-holder-resume"
+		>
+			<div
+				@click="scrollToSibling(false)"
+				class="absolute -left-5 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden hover:bg-gray-100 hover:dark:bg-gray-400 cursor-pointer select-none"
+				:class="{ 'md:block': !isExperienceActive }"
+			>
+				<chevron-left-icon
+					class="w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+				></chevron-left-icon>
+			</div>
+			<div
+				@click="scrollToSibling(true)"
+				class="absolute -right-5 rounded-full h-10 w-10 shadow-md bg-gray-50 dark:bg-gray-500 hidden hover:dark:bg-gray-400 hover:bg-gray-100 cursor-pointer select-none"
+				:class="{ 'md:block': !isSkillsActive }"
+			>
+				<chevron-right-icon
+					class="w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+				></chevron-right-icon>
+			</div>
+		</div>
+		<div
+			@click="scrollToTop"
+			class="absolute right-10 bottom-3 z-10 h-10 w-10 rounded-full shadow-md bg-gray-50 dark:bg-gray-500 cursor-pointer select-none hover:dark:bg-gray-400 hover:bg-gray-100"
+			id="top-scroller-resume"
+		>
+			<chevron-up-icon
+				class="w-6 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2"
+			></chevron-up-icon>
+		</div>
+		<div
+			class="container relative flex gap-6 overflow-x-scroll no-scrollbar snap-x snap-mandatory scroll-smooth w-full"
+			id="resume-container"
+		>
+			<div class="flex-none w-full grow snap-center resume-panels">
+				<resume-timeline
+					:ix="'first'"
+					:observer="observer"
+					:isActive="isExperienceActive"
+					:kind="'experience'"
+					id="experience"
+					class="scroll-my-10"
+					@load-complete="refreshAnimations"
+				/>
+			</div>
+			<div class="flex-none w-full grow snap-center resume-panels">
+				<resume-timeline
+					:ix="'center'"
+					:observer="observer"
+					:isActive="isEducationActive"
+					:kind="'education'"
+					id="education"
+					class="scroll-my-10"
+					@load-complete="refreshAnimations"
+				/>
+			</div>
+			<div class="flex-none w-full grow snap-center resume-panels">
+				<resume-projects
+					:ix="'center'"
+					:observer="observer"
+					id="projects"
+					:isActive="isProjectsActive"
+					class="scroll-my-10"
+					@load-complete="refreshAnimations"
+				/>
+			</div>
+			<div class="flex-none w-full grow snap-center resume-panels">
+				<resume-skills
+					:ix="'last'"
+					:observer="observer"
+					id="skills"
+					:isActive="isSkillsActive"
+					class="scroll-my-10"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import ResumeProjects from './Projects/ResumeProjects.vue';
-import ResumeSkills from './Skills/ResumeSkills.vue';
-import ResumeTimeline from './Timeline/ResumeTimeline.vue';
+import ResumeProjects from "./Projects/ResumeProjects.vue";
+import ResumeSkills from "./Skills/ResumeSkills.vue";
+import ResumeTimeline from "./Timeline/ResumeTimeline.vue";
+import {
+	ChevronLeftIcon,
+	ChevronRightIcon,
+	ChevronUpIcon,
+} from "@heroicons/vue/24/outline";
 
 export default {
-  components: { ResumeTimeline, ResumeProjects, ResumeSkills },
-  name: 'TheResume',
-  data () {
-    return {
-      t1: null,
-      t2: null,
-      t3: [],
-    }
-  },
-  props: [
-    'observer',
-    'elementsInView',
-  ],
-  inject: [
-  ],
-  computed: {
-    isExperienceActive () {
-      return this.isActive('experience')
-    },
-    isEducationActive () {
-      return this.isActive('education')
-    },
-    isProjectsActive () {
-      return this.isActive('projects')
-    },
-    isSkillsActive () {
-      return this.isActive('skills')
-    },
-  },
-  methods: {
-    scrollToTop () {
-      this.$el.scrollIntoView({behavior: 'smooth', block: 'start'});
-    },
-    isActive (kind) {
-      return this.elementsInView.filter(e => kind == e.target.id && e.isIntersecting)?.length > 0 ?? false
-    },
-    scrollToSibling (next) {
-      let scrollWidth = document.getElementById('arrow-holder-resume').clientWidth;
-      let container = document.getElementById('resume-container');
-      if (next) {
-        container.scrollLeft += scrollWidth;
-      } else {
-        container.scrollLeft -= scrollWidth;
-      }
-    },
-    setUpAnimations () {
-      const t1 = this.$gsap.timeline({
-        scrollTrigger: {
-          trigger: '#resume-container',
-          start: '10% center',
-          end: '90% center',
-          scrub: true,
-        }
-      })
-      t1
-        .to('#arrow-holder-resume', {opacity: 1, duration: 0.3})
-        .to('#arrow-holder-resume', {opacity: 0, duration: 0.3}, 0.7);
-      this.t1 = t1;
-    },
-    refreshAnimations () {
-      this.t1.scrollTrigger.refresh(true);
-    }
-  },
-  beforeUnmount () {
-    this.t1?.kill();
-  },
-  mounted () {
-    this.setUpAnimations();
-  },
-  created () {
-  },
-}
+	components: {
+		ResumeTimeline,
+		ResumeProjects,
+		ResumeSkills,
+		ChevronLeftIcon,
+		ChevronRightIcon,
+		ChevronUpIcon,
+	},
+	name: "TheResume",
+	data() {
+		return {
+			t1: null,
+			t2: null,
+			t3: [],
+		};
+	},
+	props: ["observer", "elementsInView"],
+	inject: [],
+	computed: {
+		isExperienceActive() {
+			return this.isActive("experience");
+		},
+		isEducationActive() {
+			return this.isActive("education");
+		},
+		isProjectsActive() {
+			return this.isActive("projects");
+		},
+		isSkillsActive() {
+			return this.isActive("skills");
+		},
+	},
+	methods: {
+		scrollToTop() {
+			this.$el.scrollIntoView({ behavior: "smooth", block: "start" });
+		},
+		isActive(kind) {
+			return (
+				this.elementsInView.filter(
+					(e) => kind == e.target.id && e.isIntersecting
+				)?.length > 0 ?? false
+			);
+		},
+		scrollToSibling(next) {
+			let scrollWidth = document.getElementById(
+				"arrow-holder-resume"
+			).clientWidth;
+			let container = document.getElementById("resume-container");
+			if (next) {
+				container.scrollLeft += scrollWidth;
+			} else {
+				container.scrollLeft -= scrollWidth;
+			}
+		},
+		setUpAnimations() {
+			const t1 = this.$gsap.timeline({
+				scrollTrigger: {
+					trigger: "#resume-container",
+					start: "10% center",
+					end: "90% center",
+					scrub: true,
+				},
+			});
+			t1.to("#arrow-holder-resume", { opacity: 1, duration: 0.3 }).to(
+				"#arrow-holder-resume",
+				{ opacity: 0, duration: 0.3 },
+				0.7
+			);
+			this.t1 = t1;
+		},
+		refreshAnimations() {
+			this.t1.scrollTrigger.refresh(true);
+		},
+	},
+	beforeUnmount() {
+		this.t1?.kill();
+	},
+	mounted() {
+		this.setUpAnimations();
+	},
+	created() {},
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .resume-panels {
-  @apply flex-shrink-0
+	@apply flex-shrink-0;
 }
 </style>
