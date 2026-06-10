4597702bca445dfd482664bc149d7451085b0157
lorenzosp93 <47461256+lorenzosp93@users.noreply.github.com>
2026-06-10 12:27:54 +0200

    Preserve backend API error details

--- portfolio-frontend/src/services/http.client.ts
+++ portfolio-frontend/src/services/http.client.ts
@@ -16,6 +16,40 @@ function getCSRFToken(cookieName: string): string | undefined {
   return undefined;
 }
 
+function getErrorMessage(error: AxiosError): string {
+  const data = error.response?.data as { message?: string } | undefined;
+  if (data?.message) return data.message;
+
+  if (error.response) {
+    switch (error.response.status) {
+      case 400:
+        return "Bad request";
+      case 401:
+        return "Unauthorized";
+      case 404:
+        return "Not found";
+      case 500:
+        return "Internal server error";
+      default:
+        return "An error occurred while making the request";
+    }
+  }
+
+  return "An error occurred while making the request";
+}
+
+export class ApiError extends Error {
+  status?: number;
+  data?: unknown;
+
+  constructor(message: string, status?: number, data?: unknown) {
+    super(message);
+    this.name = "ApiError";
+    this.status = status;
+    this.data = data;
+  }
+}
+
 export abstract class HttpClient {
   protected readonly instance: AxiosInstance;
 
@@ -53,25 +87,8 @@ export abstract class HttpClient {
 
   protected _handleError(error: AxiosError) {
     console.error(error);
-    if (error.response) {
-      switch (error.response.status) {
-        case 400:
-          return Promise.reject(new Error("Bad request"));
-        case 401:
-          return Promise.reject(new Error("Unauthorized"));
-        case 404:
-          return Promise.reject(new Error("Not found"));
-        case 500:
-          return Promise.reject(new Error("Internal server error"));
-        default:
-          return Promise.reject(
-            new Error("An error occurred while making the request")
-          );
-      }
-    } else {
-      return Promise.reject(
-        new Error("An error occurred while making the request")
-      );
-    }
+    return Promise.reject(
+      new ApiError(getErrorMessage(error), error.response?.status, error.response?.data)
+    );
   }
 }
