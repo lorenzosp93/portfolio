ec8047415fc1b19565a6c65282cbdc51384b57b5
lorenzosp93 <47461256+lorenzosp93@users.noreply.github.com>
2026-06-11 12:37:57 +0200

    Register system logs in admin

--- portfolio-backend/shared/admin.py
+++ portfolio-backend/shared/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import SiteSettings, Attachment
+from .models import SiteSettings, Attachment, SystemLog
 
-# Register your models here.
+
+class SystemLogAdmin(admin.ModelAdmin):
+    list_display = ("created_at", "level", "logger_name", "message")
+    list_filter = ("level", "logger_name", "created_at")
+    search_fields = ("message", "traceback", "logger_name")
+    readonly_fields = ("created_at", "level", "logger_name", "message", "traceback")
+    ordering = ("-created_at",)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
 admin.site.register(SiteSettings)
 admin.site.register(Attachment)
+admin.site.register(SystemLog, SystemLogAdmin)
