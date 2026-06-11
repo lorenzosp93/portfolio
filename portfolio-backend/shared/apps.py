d96bff4a42da16b6ce5e9ea6ae0ee36a85376a5d
lorenzosp93 <47461256+lorenzosp93@users.noreply.github.com>
2026-06-11 12:59:40 +0200

    Attach database logging handler on startup

--- portfolio-backend/shared/apps.py
+++ portfolio-backend/shared/apps.py
@@ -3,3 +3,8 @@ from django.apps import AppConfig
 
 class SharedConfig(AppConfig):
     name = 'shared'
+
+    def ready(self):
+        from .logging import attach_database_log_handler
+
+        attach_database_log_handler()
